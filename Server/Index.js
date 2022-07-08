055d2a806c02ca7a2f3635f2fadca877c1e8bc92
Mohamed Ben Taher <mbentaher1998@gmail.com>
2022-07-08 21:16:28 +0100

    make thing better

--- Server/Index.js
+++ Server/Index.js
@@ -16,7 +16,9 @@ app.use(cors());
 
 app.use('/posts',postRoutes);
 app.use('/user',userRoutes)
-
+app.get('/',(req,res) =>{ 
+    res.send("app is running");
+})
 const CONNECTION_URL=process.env.CONNECTION_URL
 const Port=process.env.port || 5000
 
