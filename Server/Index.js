0c6766bc4544dace5fbbf9249d96f026f11dd486
Mohamed Ben Taher <mbentaher1998@gmail.com>
2022-07-10 12:54:22 +0100

    make it better

--- Server/Index.js
+++ Server/Index.js
@@ -4,7 +4,8 @@ import mongoose from "mongoose";
 import cors from 'cors';
 import postRoutes from './Routes/posts.js'
 import userRoutes from './Routes/user.js'
-
+import * as dotenv from 'dotenv' 
+dotenv.config()
 const app =express();
 const PORT = process.env.PORT|| 5000;
 
@@ -16,9 +17,10 @@ app.use(cors());
 app.use('/posts',postRoutes);
 app.use('/user',userRoutes)
 app.get('/',(req,res)=>res.send('yess'))
-const CONNECTION_URL=process.env.CONNECTION_URL
-const Port=process.env.port || 5000
 
+const CONNECTION_URL=process.env.CONNECTION_URL
+const Port=process.env.port 
+console.log(CONNECTION_URL,Port)
 mongoose.connect("mongodb+srv://Medben:medben45@memories.urmfk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     .then(app.listen(Port,()=>console.log('server Running on Port :'+Port)))
      .catch((error)=>console.log(error.message));
