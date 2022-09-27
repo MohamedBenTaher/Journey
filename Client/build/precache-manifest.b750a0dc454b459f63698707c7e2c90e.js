5bdef8898ee97ec8101b1cc0cf040e5fca790e22
Mohamed Ben Taher <mbentaher1998@gmail.com>
2022-09-27 11:07:25 +0100

    build folder

---
+++ Client/build/precache-manifest.b750a0dc454b459f63698707c7e2c90e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8cd2b3b13da63da10bb490bc8416dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcd0dcad0ee939fea9d9",
+    "url": "/static/css/main.36f8a89b.chunk.css"
+  },
+  {
+    "revision": "05b2d64e15bd7805014f",
+    "url": "/static/js/2.ea1b213f.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/static/js/2.ea1b213f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcd0dcad0ee939fea9d9",
+    "url": "/static/js/main.f32a8b44.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "0ee57cfdf6f8bb9714d9badab6adbfff",
+    "url": "/static/media/title.0ee57cfd.png"
+  }
+]);
