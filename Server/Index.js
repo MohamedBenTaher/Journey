334bd6e2c3871cc2e92d6e05f8fe8e677729ecc0
Mohamed Ben Taher <mbentaher1998@gmail.com>
2022-09-28 13:42:55 +0100

    cors

--- Server/Index.js
+++ Server/Index.js
@@ -12,11 +12,8 @@ const Port=process.env.port || 5000
 
 app.use(bodyParser.json({limit:'30mb',extended:true}))
 app.use(bodyParser.urlencoded({limit:'30mb',extended:true}));
-let corsOptions = {
-    origin : ['https://joureneyfront.onrender.com'],
-}
  
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use('/posts',postRoutes);
 app.use('/user',userRoutes)
